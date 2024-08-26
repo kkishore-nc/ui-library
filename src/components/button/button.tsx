4a365ae050d173f3e41f79d089ae9a1066bfa42a
kv kishore <kanala.kishore@zensar.com>
2024-08-26 15:36:27 +0530

    delete all

--- src/components/button/button.tsx
+++
@@ -1,8 +0,0 @@
-interface IButtonProps {
-    text: string
-    onClick?: () => void;
-}
-
-export const Button = ({ text, onClick }: IButtonProps) => {
-    return <button onClick={onClick} >{text}</button>
-}
